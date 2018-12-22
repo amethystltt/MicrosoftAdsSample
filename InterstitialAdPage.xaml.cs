d9207f7c5762b895da276a266916664afb51a657
Tingting Liu <tilnl@microsoft.com>
2018-12-22 13:38:55 -0800

    add sample code

---
+++ InterstitialAdPage.xaml.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Microsoft.Advertising.WinRT.UI;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace microsoftAds
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class InterstitialAdPage : Page
+    {
+        // Assign myAppId and myAdUnitId to test values. Replace these values with live values 
+        // from Dev Center before you submit your app to the Store.
+        InterstitialAd myInterstitialAd = null;
+        string myAppId = "d25517cb-12d4-4699-8bdc-52040c712cab";
+        string myAdUnitId = "test";
+
+        public InterstitialAdPage()
+        {
+            this.InitializeComponent();
+
+            myInterstitialAd = new InterstitialAd();
+            myInterstitialAd.AdReady += MyInterstitialAd_AdReady;
+            myInterstitialAd.ErrorOccurred += MyInterstitialAd_ErrorOccurred;
+            myInterstitialAd.Completed += MyInterstitialAd_Completed;
+            myInterstitialAd.Cancelled += MyInterstitialAd_Cancelled;
+
+            if (MainPage.isVideoAd)
+            {
+                myInterstitialAd.RequestAd(AdType.Video, myAppId, myAdUnitId);
+            }
+            else
+            {
+                myInterstitialAd.RequestAd(AdType.Display, myAppId, myAdUnitId);
+            }
+
+            if (InterstitialAdState.Ready == myInterstitialAd.State)
+            {
+                myInterstitialAd.Show();
+            }
+        }
+
+        private void requestAdButton_Click(object sender, RoutedEventArgs e)
+        {
+            myInterstitialAd.RequestAd(AdType.Video, myAppId, myAdUnitId);
+        }
+
+        // This method attempts to show the interstitial ad when the "Show ad" button is clicked.
+        private void showAdButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (InterstitialAdState.Ready == myInterstitialAd.State)
+            {
+                myInterstitialAd.Show();
+            }
+        }
+
+        void MyInterstitialAd_AdReady(object sender, object e)
+        {
+            myInterstitialAd.Show();
+        }
+
+        void MyInterstitialAd_ErrorOccurred(object sender, AdErrorEventArgs e)
+        {
+            // Your code goes here.
+        }
+
+        void MyInterstitialAd_Completed(object sender, object e)
+        {
+            Frame.Navigate(typeof(MainPage));
+        }
+
+        void MyInterstitialAd_Cancelled(object sender, object e)
+        {
+            Frame.Navigate(typeof(MainPage));
+        }
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace microsoftAds
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public static bool isVideoAd = false;
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Interval = new TimeSpan(0, 0, 1);
+            timer.Tick += dispatcherTimer_Tick;
+            timer.Start();
+        }
+
+        void dispatcherTimer_Tick(object sender, object e)
+        {
+            this.textBlock1.Text = DateTime.Now.ToString("HH:mm:ss");
+        }
+
+        private void NativeAdsBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Frame.Navigate(typeof(NativeAdPage));
+        }
+
+        private void InterstitialVideoAdsBtn_Click(object sender, RoutedEventArgs e)
+        {
+            isVideoAd = true;
+            Frame.Navigate(typeof(InterstitialAdPage));
+        }
+
+        private void InterstitialDisplayAdsBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Frame.Navigate(typeof(InterstitialAdPage));
+        }
+    }
+}
